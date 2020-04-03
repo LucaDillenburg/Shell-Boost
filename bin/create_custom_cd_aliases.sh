70a8a142d626772379974f427608abacb2149504
Luca Dillenburg <luca.assumpcao.dillenburg@gmail.com>
2020-04-02 22:15:41 -0300

    create custom cd alias writing to file

--- bin/create_custom_cd_aliases.sh
+++ bin/create_custom_cd_aliases.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
 
-function create_custom_cd_aliases {
-	#echo "Do you want to append the existing custom"
-	echo $(__bold "You can press Ctrl+C (or Command+. for macOS) at any moment to stop creating aliases. The aliases already created won't be lost.")
+function __get_file_path_custom_aliases {
+	echo "$(__get_data_folder)/custom_aliases.sh"
+}
+function __overwrite_custom_aliases_file {
+	echo "#!/bin/bash" > $(__get_file_path_custom_aliases)
+}
 
-	initial_pwd=$(pwd)
+if ! [[ -f $(__get_file_path_custom_aliases) ]]; then
+	touch $(__get_file_path_custom_aliases)
+	__overwrite_custom_aliases_file
+fi
+source $(__get_file_path_custom_aliases)
+
+function create_custom_cd_aliases {
+	if ! [[ -z $(grep alias $(__get_file_path_custom_aliases)) ]]; then
+		printf "Do you want to overwrite the commands already created by this command? (if you choose NO the aliases will be appended) [y/N] "
+		read -r response
+		if [[ response == "y" || response == "Y" ]]; then
+			__overwrite_custom_aliases_file
+			echo "Unless you create other aliases with the names of the old ones, they will still be accessible in this terminal session. The next time you open a terminal window or tab they won't be accessible anymore."
+		fi
+	fi
+
+	echo "You can press Ctrl+C (or Command+. for macOS) when you don't want to create more aliases."
+	echo ""
 
 	while true
 	do
@@ -12,9 +32,9 @@ function create_custom_cd_aliases {
 		while true
 		do
 			printf "Enter directory path (relative or absolute): "
-			read -r directory
+			read -r directory #todo directory auto-completition
 
-			#directory=$(__get_absolute_path $directory)
+			directory=$(__get_absolute_path $directory)
 			if ! [[ -d $directory ]]; then
 				__echo_error "No such directory: $directory"
 			else
@@ -25,20 +45,25 @@ function create_custom_cd_aliases {
 		_alias=""
 		while true
 		do
-			printf "Enter alias (keep it short):"
+			printf "Enter alias (keep it short): "
 			read -r _alias
 
-			if [[ -z $_alias ]]; then
-				__echo_error "The alias cannot be empty."
+			if ! [[ $_alias =~ '^[_a-zA-Z][_a-zA-Z0-9\-]*$' ]]; then
+				__echo_error "Invalid alias. Alias name rules:"
+				__echo_error "1. It has to start with a letter or underline."
+				__echo_error "2. It can only have letters, numbers, underlines and hyphens."
+				echo ""
 			else
 				break
 			fi
 		done
 
-		echo "$_alias alias created: alias $_alias='$directory'"
-		#todo
+		alias_command="alias $_alias='$directory'"
+		echo "$_alias alias created: $alias_command"
+		echo $alias_command >> $(__get_file_path_custom_aliases)
 
-		printf "Do you want to create another custom alias?[Y/n]"
+		echo ""
+		printf "Do you want to create another custom alias? [Y/n] "
 		read -r response
 		if [[ $response == "n" || $response == "N" ]]; then
 			break
@@ -46,5 +71,5 @@ function create_custom_cd_aliases {
 	done
 
 	echo "Have a nice day :D "
-	#source $SH_NAV_HOME/bin/custom_aliases.sh
+	source $(__get_file_path_custom_aliases)
 }
