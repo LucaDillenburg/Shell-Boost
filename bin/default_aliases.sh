63f694da142233d658dac83583cab5f51d15c12e
Luca Dillenburg <luca.assumpcao.dillenburg@gmail.com>
2020-04-04 23:19:21 -0300

    more aliases created

--- bin/default_aliases.sh
+++ bin/default_aliases.sh
@@ -2,3 +2,11 @@
 
 alias c='clear;'
 alias ..='cd ..;'
+alias ee='exit'
+
+alias untar='tar xf '
+
+alias gac='git add .; git commit -m '
+alias gst='git status '
+alias glg='git log '
+alias gpm='git push origin master '
