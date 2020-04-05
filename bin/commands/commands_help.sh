fd593ac6ba929689e6fce90a148ab9df5359aa2f
Luca Dillenburg <luca.assumpcao.dillenburg@gmail.com>
2020-04-04 23:18:55 -0300

    change commands --help

--- bin/commands/commands_help.sh
+++ bin/commands/commands_help.sh
@@ -3,7 +3,7 @@
 function __echo_cd_help {
 	echo -e "Usage: cd DIRECTORY"
 	echo -e "Navigate to the DIRECTORY, if it exist and you have the right permissions."
-    echo -e "	-h, --help 	display this help"
+    echo -e "	-h, --help 	display command description"
 	__echo_check_our_repo
 }
 
@@ -11,7 +11,7 @@ function __echo_back_help {
 	echo -e "Usage: back [OPTION]"
 	echo -e "Navigate backwards in the directory history."
 	echo -e "	-n=NUMBER	navigate NUMBER times backwards"
-    echo -e "	-h, --help 	display this help"
+    echo -e "	-h, --help 	display command description"
 	__echo_check_our_repo
 }
 
@@ -20,14 +20,21 @@ function __echo_fwd_help {
 	echo -e "Navigate forward in the directory history."
 	echo -e "	-n=NUMBER	navigate NUMBER times forward"
 	echo -e "	-a, --all	navigate forward until the last directory in the history"
-    echo -e "	-h, --help 	display this help"
+    echo -e "	-h, --help 	display command description"
 	__echo_check_our_repo
 }
 
 function __echo_mkcd_help {
 	echo -e "Usage: mkcd DIRECTORY."
-	echo -e "Create directory and navigate to it."	
-    echo -e "	-h, --help 	display this help"
+	echo -e "Create directory and navigate to it."
+    echo -e "	-h, --help 	display command description"
+	__echo_check_our_repo
+}
+
+function __echo_create_cd_alias {
+	echo -e "Usage: create_custom_cd_aliases."
+	echo -e "Create custom cd aliases interactively."
+    echo -e "	-h, --help 	display command description"
 	__echo_check_our_repo
 }
 
