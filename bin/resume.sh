da6258283daec92ae29e4efb9f670d135d1233dd
Luca Dillenburg <luca.assumpcao.dillenburg@gmail.com>
2020-04-08 08:26:43 -0300

    update resume.sh

--- bin/resume.sh
+++ bin/resume.sh
@@ -17,26 +17,25 @@ function __ask_show_all_commands {
 }
 
 function __all_commands_resume {
-	echo $(__bold "Available aliases:")
-	echo " "
-	echo $(__bold "# General aliases")
-	__echo_with_tab "- $(__bold "c") -> clear screen"
-	__echo_with_tab "- $(__bold "..") -> cd .."
-	__echo_with_tab "- $(__bold "ee") -> exit terminal"
-	__echo_with_tab "- $(__bold "untar [FILE]") -> untar .tar[.gz] file"
-	echo " "
-	echo $(__bold "# Git")
-	__echo_with_tab "- $(__bold "gac [COMMIT-MESSAGE]") -> stages all of the files in the current directory and commit with the message passed by argument"
-	__echo_with_tab "- $(__bold "gst") -> git status"
-	__echo_with_tab "- $(__bold "glg") -> git log"
-	__echo_with_tab "- $(__bold "gpm") -> git push origin master"
-
-	echo " "
 	echo $(__bold "Available commands:")
 	__echo_with_tab "- $(__bold "back") -> navigate backwards in the directory history"
 	__echo_with_tab "- $(__bold "fwd")  -> navigate forward in the directory history"
 	__echo_with_tab "- $(__bold "mkcd") -> create directory and navigate to it"
 	echo "For more information about this commands execute \"command-name --help\"."
+
+	echo " "
+
+	echo $(__bold "Available aliases:")
+	echo $(__bold " # General aliases")
+	__echo_with_tab "- $(__bold "c") -> clear screen"
+	__echo_with_tab "- $(__bold "..") -> cd .."
+	__echo_with_tab "- $(__bold "ee") -> exit terminal"
+	__echo_with_tab "- $(__bold "untar [FILE]") -> untar .tar[.gz] file"
+	echo $(__bold " # Git")
+	__echo_with_tab "- $(__bold "gac [COMMIT-MESSAGE]") -> stages all of the files in the current directory and commit with the message passed by argument"
+	__echo_with_tab "- $(__bold "gst") -> git status"
+	__echo_with_tab "- $(__bold "glg") -> git log"
+	__echo_with_tab "- $(__bold "gpm") -> git push origin master"	
 }
 
 function __echo_with_tab {
